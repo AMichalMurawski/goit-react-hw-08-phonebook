11e2a877ad1fd56dd155223cbd76764ff7bbf5e1
Michał Murawski <m.a.murawski@wp.pl>
2023-01-19 11:51:26 +0100

    change redux

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,42 +1,69 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchContacts, addContact, deleteContact } from "./operations";
 
-const LOCAL_STORAGE_CONTACTS = "phonebookContacts";
+const handlePending = state => {
+    state.isLoading = true;
+}
 
-const contactsInitialState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_CONTACTS));
+const handleRejected = (state, action) => {
+    state.isLoading = false;
+    state.error = action.payload;
+}
 
 const contactsSlice = createSlice({
     name: "contacts",
-    initialState: contactsInitialState,
-    reducers: {
-        addContact: {
-            reducer(state, action) {
-                state.push(action.payload)
-                setLocalStorage([...state])
-            },
-            prepare(text) {
-                return {
-                    payload: {
-                        id: nanoid(),
-                        name: text.name,
-                        number: text.number,
-                    }
-                }
-            }
+    initialState: {
+        items: [],
+        isLoading: false,
+        error: null,
+    },
+    extraReducers: {
+        [fetchContacts.pending]: handlePending,
+        [fetchContacts.fulfilled](state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.items = action.payload;
         },
+        [fetchContacts.rejected]: handleRejected,
+        [addContact.pending]: handlePending,
+        [addContact.fulfilled](state, action) {
+            state.isLoading = false;
+            state.error = null;
+            state.items.push(action.payload);
+        },
+        [addContact.rejected]: handleRejected,
+        [deleteContact.pending]: handlePending,
+        [deleteContact.fulfilled](state, action) {
+            state.isLoading = false;
+            state.error = null;
+            const index = state.items.findIndex(contact => contact.id === action.payload);
+            state.items.splice(index, 1);
+        },
+        [deleteContact.rejected]: handleRejected,
+    },
+    reducers: {
+        // addContact: {
+        //     reducer(state, action) {
+        //         state.push(action.payload)
+        //     },
+        //     prepare(text) {
+        //         return {
+        //             payload: {
+        //                 id: nanoid(),
+        //                 name: text.name,
+        //                 number: text.number,
+        //             }
+        //         }
+        //     }
+        // },
         deleteContact: {
             reducer(state, action) {
                 const index = state.findIndex(contact => contact.id === action.payload);
                 state.splice(index, 1);
-                setLocalStorage([...state])
             },
         }
     }
 })
 
-export const { addContact, deleteContact } = contactsSlice.actions;
 export const contactsReducer = contactsSlice.reducer;
-
-const setLocalStorage = (contacts) => {
-    localStorage.setItem(LOCAL_STORAGE_CONTACTS, JSON.stringify(contacts))
-}
 
