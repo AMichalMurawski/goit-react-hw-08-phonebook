4496d7b8d90c77764287e5fc6e3b9cde6455a218
Michał Murawski <m.a.murawski@wp.pl>
2023-01-21 00:57:54 +0100

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { fetchContacts } from 'redux/operations';
 import { SharedLayout } from 'components/SharedLayout/SharedLayout';
 
+const Login = lazy(() => import('./pages/Login'));
 const Contacts = lazy(() => import('./pages/Contacts'));
 
 export const App = () => {
@@ -18,7 +19,7 @@ export const App = () => {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route path="/contacts" element={<Contacts />} />
-
+        <Route path="/login" element={<Login />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
---
+++ src/pages/Login.jsx
@@ -0,0 +1,38 @@
+import css from './Login.module.css';
+
+const Login = () => {
+  const handleSubmit = e => {};
+  return (
+    <main>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <label className={css.label}>
+          E-mail / Name
+          <input
+            className={css.input}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+        <label className={css.label}>
+          Password
+          <input
+            className={css.input}
+            type="password"
+            name="password"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </label>
+        <button className={css.button} type="submit">
+          Login
+        </button>
+      </form>
+    </main>
+  );
+};
+
+export default Login;
