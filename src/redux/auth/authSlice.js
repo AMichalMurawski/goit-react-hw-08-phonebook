35b81c608c63cfc1be9c4ef799c643c54fc1f607
Michał Murawski <m.a.murawski@wp.pl>
2023-01-25 10:58:04 +0100

    complete redux auth

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { act } from 'react-dom/test-utils';
+import { register, logIn, logOut, refreshUser } from './authThunk';
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: builder => {
+    builder.addCase(register.fulfilled, (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    });
+    builder.addCase(logIn.fulfilled, (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    });
+    builder.addCase(logOut.fulfilled, (state, action) => {
+      state.user = { name: null, email: null };
+      state.token = null;
+      isLoggedIn = false;
+    });
+    builder
+      .addCase(refreshUser.pending, state => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, state => {
+        state.isRefreshing = false;
+      });
+  },
+});
+
+export const authReducer = authSlice.reducer;
